4dec8417e67ed0a7a280aa6795acf86d2445ab14
Ben Thomson <git@alfreido.com>
2025-04-02 08:37:12 +0800

    Add 1Password RPM to install

--- build.sh
+++ build.sh
@@ -11,11 +11,15 @@ RELEASE="$(rpm -E %fedora)"
 # List of rpmfusion packages can be found here:
 # https://mirrors.rpmfusion.org/mirrorlist?path=free/fedora/updates/39/x86_64/repoview/index.html&protocol=https&redirect=1
 
-# this installs a package from fedora repos
+# Install Evolution, EWS and legacy iptables
+# Note: iptables-legacy is needed for Docker-in-Docker compatibility within VSCode
 rpm-ostree install \
     evolution \
     evolution-ews \
     iptables-legacy
+
+# Install 1Password
+rpm-ostree install https://downloads.1password.com/linux/rpm/stable/x86_64/1password-latest.rpm
 
 #### Example for enabling a System Unit File
 systemctl enable podman.socket
