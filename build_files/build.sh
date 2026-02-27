35a17cdfe8a29cb0c12608bca07e24d714f0f42f
Ben Thomson <git@alfreido.com>
2026-02-27 09:14:46 +0800

    Synchronise with base image repo

--- build_files/build.sh
+++ build_files/build.sh
@@ -7,7 +7,7 @@ set -ouex pipefail
 # Packages can be installed from any enabled yum repo on the image.
 # RPMfusion repos are available by default in ublue main images
 # List of rpmfusion packages can be found here:
-# https://mirrors.rpmfusion.org/mirrorlist?path=free/fedora/updates/39/x86_64/repoview/index.html&protocol=https&redirect=1
+# https://mirrors.rpmfusion.org/mirrorlist?path=free/fedora/updates/43/x86_64/repoview/index.html&protocol=https&redirect=1
 
 # Install Evolution, EWS and legacy iptables
 # Note: iptables-legacy is needed for Docker-in-Docker compatibility within VSCode
